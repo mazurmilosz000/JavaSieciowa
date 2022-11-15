e3bafd6140a047e33b3a36b9d8961dad90a35d2d
Milosz Mazur <mazur.milosz000@gmail.com>
2022-11-16 00:37:36 +0100

    resolved conflict

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
